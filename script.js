c14e747ff2db755d29c5f084c3c9cbfa73b16b23
Gbohunmi Williams <elitegbohunmi@gmail.com>
2025-01-15 05:24:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+    distance: '60px',
+    opacity: '0'
+});
+ScrollReveal().reveal('.hmm', { delay: 100,  origin:'left', reset:true});
+ScrollReveal().reveal('.sec', { delay: 300,  origin:'top', });
+ScrollReveal().reveal('.third', { delay: 300,  origin:'top', });
+ScrollReveal().reveal('.mission', { delay: 300,  origin:'top', });
+ScrollReveal().reveal('.vision', { delay: 400,  origin:'top', });
+ScrollReveal().reveal('.cardm', { delay: 400,  origin:'left', });
+ScrollReveal().reveal('.card', { delay: 400,  origin:'left', });
