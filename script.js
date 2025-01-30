528e2f2bbf9c18d006a4fa91c91198b11fd5e220
Gbohunmi Williams <elitegbohunmi@gmail.com>
2025-01-30 20:27:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ ScrollReveal().reveal('.mission', { delay: 300,  origin:'top', });
 ScrollReveal().reveal('.vision', { delay: 400,  origin:'top', });
 ScrollReveal().reveal('.cardm', { delay: 400,  origin:'left', });
 ScrollReveal().reveal('.card', { delay: 400,  origin:'left', });
+
+function toggleMenu() {
+
+  const menuIcon = document.querySelector(".menu-icon");
+
+  menuIcon.classList.toggle("active");
+
+  document.querySelector("ul").classList.toggle("yh");
+
+}
